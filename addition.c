4e16e66f6a3d3f4f1f8e340bdfb41e4ef163a628
divyanshbhatia1509 <divyanshbhatiabjr@gmail.com>
2025-08-13 10:43:32 +0530

    Add files via upload

---
+++ addition.c
@@ -0,0 +1,12 @@
+# include<stdio.h>
+int main (){
+int a,b,c,d,e;
+printf("enter the value of four numbers");
+scanf("%d", &a);
+scanf("%d", &b);
+scanf("%d", &d);
+scanf("%d",&e);
+c = a+b+d+e;
+printf("the sum of four numbers is %d",c);
+return 0;
+}
